17799476edecd4160fcf1455a347026a2b7424d5
Bas Bakens <76151181+Bash-04@users.noreply.github.com>
2022-06-24 00:44:25 +0200

    added some comments in 'dotmenu.js'

--- web/src/function/dotmenu.js
+++ web/src/function/dotmenu.js
@@ -1,5 +1,7 @@
+//click on dotmenu to open
 document.getElementById("dotmenu").addEventListener("click", openmenu)
 
+//open menu function
 function openmenu(){
     var x = document.getElementById("menu");
     if (x.style.display = "none") {
@@ -7,10 +9,10 @@ function openmenu(){
     }
 } 
 
-//click outside menu listener
+//dblclick outside menu listener
 addEventListener("dblclick", close);
 
-//close menu
+//close menu function
 function close(){
     var x = document.getElementById("menu");
     if (x.style.display = "flex") {
