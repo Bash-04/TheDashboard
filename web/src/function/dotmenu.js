8314e4c81f822e9906fe83006fdd166a7c40955a
Bas Bakens <76151181+Bash-04@users.noreply.github.com>
2022-06-24 00:41:59 +0200

    dotmenu top right - dashboard 2 - open/close fix
    after adding the dotmenu in the top right it wouldn't work.
    
    added .addEventListener("click") to open the menu and added .addEventListener("dblclick") to close the menu.

--- web/src/function/dotmenu.js
+++ web/src/function/dotmenu.js
@@ -1,12 +1,19 @@
-const dot = document.getElementsByClassName("dotmenu");
-const menu = document.getElementsByClassName("menu");
+document.getElementById("dotmenu").addEventListener("click", openmenu)
 
-addEventListener("click", displ);
-
-function displ(){
-    if (menu.style.display === "none") {
-        menu.style.display = "flex";
-    } else {
-        menu.style.display = "none";
+function openmenu(){
+    var x = document.getElementById("menu");
+    if (x.style.display = "none") {
+        x.style.display = "flex";
     }
+} 
+
+//click outside menu listener
+addEventListener("dblclick", close);
+
+//close menu
+function close(){
+    var x = document.getElementById("menu");
+    if (x.style.display = "flex") {
+        x.style.display = "none";
+      }
 }
