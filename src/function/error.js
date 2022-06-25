73d1a9ddaaee7165c0114fdfae145b9597a3a32d
Bas Bakens <76151181+Bash-04@users.noreply.github.com>
2022-06-25 18:31:23 +0200

    Delete src directory

--- src/function/error.js
+++
@@ -1,4 +0,0 @@
-//script to relocate someone from a error to the homepage
-setTimeout(function(){
-    window.location.href = '/';
-}, 15000);
--- src/function/localdate.js
+++
@@ -1,7 +0,0 @@
-const maanden = [" Januari"," Februari"," Maart"," April"," Mei"," Juni"," Juli"," Augustus"," September"," Oktober"," November"," December"];
-const dagen = ["Zondag ", "Maandag ", "Dinsdag ", "Woensdag ", "Donderdag ", "Vrijdag ", "Zaterdag "]
-
-const d = new Date();
-let day = dagen[d.getDay()];
-let month = maanden[d.getMonth()];
-document.getElementById("date").innerHTML = day + d.getDate() + month;
