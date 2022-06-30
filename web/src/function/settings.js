e8e6693770eae3f175e9e1b47814b809f5c6dbd9
Bas Bakens <76151181+Bash-04@users.noreply.github.com>
2022-07-01 00:52:59 +0200

    working on settings
    added mid-section for the settings
    added basic sidebar
    added basic settings-display
    added onclick event per sidebar div to settings.js

--- web/src/function/settings.js
+++ web/src/function/settings.js
@@ -14,6 +14,141 @@ document.getElementById("closesettings").addEventListener("click", closesettings
 function closesettings(){
     var x = document.getElementById("settings")
     if (x.style.display = "block") {
-        x.style.display = "none"
+        x.style.display = "none";
     }
 }
+
+
+
+// setting display
+    //addEventListener click
+    document.getElementById("open-background-changer").addEventListener("click", openbackgroundchanger)
+    document.getElementById("open-appearancetwo").addEventListener("click", openappearancetwo)
+    document.getElementById("open-appearancethree").addEventListener("click", openappearancethree)
+    document.getElementById("open-reset-settings").addEventListener("click", openresetsettings)
+    document.getElementById("open-dev").addEventListener("click", opendev)
+
+        //background-changer
+        function openbackgroundchanger(){
+            var bc = document.getElementById("background-changer")
+            var at = document.getElementById("appearancetwo")
+            var ath = document.getElementById("appearancethree")
+            var rs = document.getElementById("reset-settings")
+            var dev = document.getElementById("dev")
+
+            if (bc.style.display = "none") {
+                bc.style.display = "block";
+            }
+            if (at.style.display = "block") {
+                at.style.display = "none";
+            }
+            if (ath.style.display = "block") {
+                ath.style.display = "none";
+            }
+            if (rs.style.display = "block") {
+                rs.style.display = "none";
+            }
+            if (dev.style.display = "block") {
+                dev.style.display = "none";
+            }
+        }
+
+        //appearance 2
+        function openappearancetwo(){
+            var bc = document.getElementById("background-changer")
+            var at = document.getElementById("appearancetwo")
+            var ath = document.getElementById("appearancethree")
+            var rs = document.getElementById("reset-settings")
+            var dev = document.getElementById("dev")
+            
+            if (bc.style.display = "block") {
+                bc.style.display = "none";
+            }
+            if (at.style.display = "none") {
+                at.style.display = "block";
+            }
+            if (ath.style.display = "block") {
+                ath.style.display = "none";
+            }
+            if (rs.style.display = "block") {
+                rs.style.display = "none";
+            }
+            if (dev.style.display = "block") {
+                dev.style.display = "none";
+            }
+        }
+
+        //appearance 3
+        function openappearancethree(){
+            var bc = document.getElementById("background-changer")
+            var at = document.getElementById("appearancetwo")
+            var ath = document.getElementById("appearancethree")
+            var rs = document.getElementById("reset-settings")
+            var dev = document.getElementById("dev")
+
+            if (bc.style.display = "block") {
+                bc.style.display = "none";
+            }
+            if (at.style.display = "block") {
+                at.style.display = "none";
+            }
+            if (ath.style.display = "none") {
+                ath.style.display = "block";
+            }
+            if (rs.style.display = "block") {
+                rs.style.display = "none";
+            }
+            if (dev.style.display = "block") {
+                dev.style.display = "none";
+            }
+        }
+
+        //reset-settings
+        function openresetsettings(){
+            var bc = document.getElementById("background-changer")
+            var at = document.getElementById("appearancetwo")
+            var ath = document.getElementById("appearancethree")
+            var rs = document.getElementById("reset-settings")
+            var dev = document.getElementById("dev")
+            
+            if (bc.style.display = "block") {
+                bc.style.display = "none";
+            }
+            if (at.style.display = "block") {
+                at.style.display = "none";
+            }
+            if (ath.style.display = "block") {
+                ath.style.display = "none";
+            }
+            if (rs.style.display = "none") {
+                rs.style.display = "block";
+            }
+            if (dev.style.display = "block") {
+                dev.style.display = "none";
+            }
+        }
+
+        //dev
+        function opendev(){
+            var bc = document.getElementById("background-changer")
+            var at = document.getElementById("appearancetwo")
+            var ath = document.getElementById("appearancethree")
+            var rs = document.getElementById("reset-settings")
+            var dev = document.getElementById("dev")
+            
+            if (bc.style.display = "block") {
+                bc.style.display = "none";
+            }
+            if (at.style.display = "block") {
+                at.style.display = "none";
+            }
+            if (ath.style.display = "block") {
+                ath.style.display = "none";
+            }
+            if (rs.style.display = "block") {
+                rs.style.display = "none";
+            }
+            if (dev.style.display = "none") {
+                dev.style.display = "block";
+            }
+        }
